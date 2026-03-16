432770c772bf1b94ab6cdbb1e1e3cb99e1d1c56b
Alisha1904 <alishaswain15@gmail.com>
2026-03-16 09:46:20 +0530

    Update Room classes and main app; remove Main.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -3,24 +3,6 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-class Room {
-    private String type;
-    private int beds;
-    private int size;
-    private double price;
-
-    public Room(String type, int beds, int size, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-
-    public String getType() { return type; }
-    public int getBeds() { return beds; }
-    public int getSize() { return size; }
-    public double getPrice() { return price; }
-}
 
 class RoomInventory {
     private Map<String, Integer> roomAvailability;
---
+++ src/SingleRoom.java
@@ -0,0 +1,5 @@
+public class SingleRoom extends Room{
+    public SingleRoom(String type, int beds, int size, double price) {
+        super(type, beds, size, price);
+    }
+}
